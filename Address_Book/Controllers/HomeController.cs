095ec69278e695d203b6cd75345dd90d65a479a6
dawidciula <84280914+dawidciula@users.noreply.github.com>
2024-05-22 18:50:29 +0200

    view, model and controller added

--- Address_Book/Controllers/HomeController.cs
+++ Address_Book/Controllers/HomeController.cs
@@ -1,31 +1,65 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
 using Address_Book.Models;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Address_Book.Controllers;
-
-public class HomeController : Controller
+namespace Address_Book.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly List<AddressBookEntry> _addressBook = new List<AddressBookEntry>();
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        // POST: /AddressBook/AddAddress
+        [HttpPost]
+        public IActionResult AddAddress(AddressBookEntry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                entry.Id = _addressBook.Count + 1;
+                _addressBook.Add(entry);
+                return RedirectToAction("Index");
+            }
+            return View("Index", entry);
+        }
+
+        // GET: /AddressBook/GetLastAddress
+        [HttpGet]
+        public IActionResult GetLastAddress()
+        {
+            if (_addressBook.Count > 0)
+            {
+                var lastAddress = _addressBook[_addressBook.Count - 1];
+                return Ok(lastAddress);
+            }
+            return NotFound();
+        }
+
+        // GET: /AddressBook/GetAddressesByCity/{city}
+        [HttpGet("{city}")]
+        public IActionResult GetAddressesByCity(string city)
+        {
+            var addressesInCity = new List<AddressBookEntry>();
+            foreach (var entry in _addressBook)
+            {
+                if (entry.Address.Contains(city, StringComparison.OrdinalIgnoreCase))
+                {
+                    addressesInCity.Add(entry);
+                }
+            }
+
+            if (addressesInCity.Count > 0)
+            {
+                return Ok(addressesInCity);
+            }
+            return NotFound();
+        }
     }
 }
